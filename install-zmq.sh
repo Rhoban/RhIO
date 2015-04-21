9109221e70afae30eb8892396db5d8e4d168af2c
Gregwar <g.passault@gmail.com>
2015-04-21 10:42:45 +0200

    Script to get zeromq

---
+++ install-zmq.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+dir=$PWD
+rm -rf zeromq-4.0.5*
+rm -rf ZeroMQ
+rm -rf cppzmq
+
+wget http://download.zeromq.org/zeromq-4.0.5.tar.gz &&
+tar zxvf zeromq-4.0.5.tar.gz &&
+cd zeromq-4.0.5 &&
+./configure --prefix=$dir/ZeroMQ &&
+make &&
+make install &&
+rm -rf zeromq-4.0.5.tar.gz
+cd ..
+
+echo "Installing CPPZMQ..." &&
+git clone https://github.com/zeromq/cppzmq.git &&
+ln -s cppzmq/zmq.hpp ZeroMQ/include/zmq.hpp
