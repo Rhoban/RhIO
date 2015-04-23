1d62b2eabd153c9e4ac3e43e89cba73632294976
Gregwar <g.passault@gmail.com>
2015-04-23 17:23:04 +0200

    Adding Plot command

---
+++ Shell/commands/PlotCommand.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <iostream>
+#include "Command.h"
+#include <Value.hpp>
+
+namespace RhIO
+{
+    class GnuPlot;
+    class NodePool;
+    class PlotCommand : public Command
+    {
+        public:
+            virtual std::string getName();
+            virtual std::string getDesc();
+            virtual void process(std::vector<std::string> args);
+
+            void update(GnuPlot *plot, NodePool *pool);
+    };
+}
